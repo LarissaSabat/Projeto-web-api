0ce1abbd2189a524575e92e4d9b5b8fe197fd8a7
Larissa Batista <147754024+LarissaSabat@users.noreply.github.com>
2024-06-07 14:14:40 -0300

    Update script.js

--- Projeto WEB API Java/script.js
+++ Projeto WEB API Java/script.js
@@ -1,3 +1,20 @@
+function salva () {
+
+    fetch("http://localhost:8080/formulario",
+        {
+            headers:{
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({a: 1, b:2 })
+    })
+        .then(function (res) {console.log(res)})
+        .catch(function (res) {console.log (res)})
+    
+};
+
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('formu').addEventListener('submit', function(event) {
         event.preventDefault(); 
